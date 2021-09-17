9d086d5ce55126e29bfa26f85a2485de1919b33f
santiago cortes <jeissoncortes02@outlook.com>
2021-09-17 10:35:24 -0500

    Creaicon del modelo entidad Todo

--- src/main/java/com/jeiss/todo/model/Todo.java
+++ src/main/java/com/jeiss/todo/model/Todo.java
@@ -1,6 +1,38 @@
 package com.jeiss.todo.model;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Todo implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private ZonedDateTime startDate;
+
+    private boolean done;
+
+    private boolean favorite;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "categoryId")
+    private Category category;
+
 }
