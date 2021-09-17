c945ef64e371cde8f60682014b7ae1acc3fea80b
santiago cortes <jeissoncortes02@outlook.com>
2021-09-17 10:44:01 -0500

    Creacion del model entidad User y las relaciones entre esas entidades

--- src/main/java/com/jeiss/todo/model/Category.java
+++ src/main/java/com/jeiss/todo/model/Category.java
@@ -1,12 +1,13 @@
 package com.jeiss.todo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -21,5 +22,13 @@ public class Category implements Serializable {
     private String name;
 
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @JsonIgnore
+    private User user;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    private List<Todo> todoList;
 
 }
