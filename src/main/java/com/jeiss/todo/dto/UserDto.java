c4a21bbbce6fab5e9666786e7d9458e02b7733fb
santiago cortes <jeissoncortes02@outlook.com>
2021-09-18 16:39:19 -0500

    dtos completados

--- src/main/java/com/jeiss/todo/dto/UserDto.java
+++ src/main/java/com/jeiss/todo/dto/UserDto.java
@@ -1,4 +1,30 @@
 package com.jeiss.todo.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDto {
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String userName;
+
+    private String password;
+
+    @JsonIgnore
+    private List<CategoryDto> category;
 }
