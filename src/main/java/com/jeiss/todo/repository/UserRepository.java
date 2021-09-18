ffe9beeef556093499c657cdfde823777fcd418b
santiago cortes <jeissoncortes02@outlook.com>
2021-09-18 16:15:46 -0500

    repositorio para el usuario y una query de buscar usuaario por email y passowrd usando queryMethods

---
+++ src/main/java/com/jeiss/todo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jeiss.todo.repository;
+
+import com.jeiss.todo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findUserByEmailAndPassword(String email, String password);
+}
