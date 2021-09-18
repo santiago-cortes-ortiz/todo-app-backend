86c89073b41872e81c83157150f01bb54652a76b
santiago cortes <jeissoncortes02@outlook.com>
2021-09-18 16:34:47 -0500

    atributos para el ndto de category

--- src/main/java/com/jeiss/todo/dto/CategoryDto.java
+++ src/main/java/com/jeiss/todo/dto/CategoryDto.java
@@ -5,9 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class CategoryDto {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private UserDto user;
+
+    private List<TodoDto> todoList;
+
 }
