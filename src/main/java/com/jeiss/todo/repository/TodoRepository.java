3b3162819647f7161f47186fe45a68d104567a09
santiago cortes <jeissoncortes02@outlook.com>
2021-09-18 16:19:29 -0500

    repositorio de todo y query para recuperar lista busscada por la categoria

--- src/main/java/com/jeiss/todo/repository/TodoRepository.java
+++ src/main/java/com/jeiss/todo/repository/TodoRepository.java
@@ -4,7 +4,6 @@ import com.jeiss.todo.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo,Long> {
     List<Todo> findTodoByCategoryId(Long categoryId);
