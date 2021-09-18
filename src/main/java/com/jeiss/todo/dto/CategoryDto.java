683c9e4b97083497f76b9acb36881622ec35b0da
santiago cortes <jeissoncortes02@outlook.com>
2021-09-18 16:33:30 -0500

    atrubutos del dto

--- src/main/java/com/jeiss/todo/dto/CategoryDto.java
+++ src/main/java/com/jeiss/todo/dto/CategoryDto.java
@@ -1,4 +1,13 @@
 package com.jeiss.todo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CategoryDto {
 }
--- src/main/java/com/jeiss/todo/dto/TodoDto.java
+++ src/main/java/com/jeiss/todo/dto/TodoDto.java
@@ -1,4 +1,29 @@
 package com.jeiss.todo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TodoDto {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private ZonedDateTime startDate;
+
+    private boolean done;
+
+    private boolean favorite;
+
+    private CategoryDto category;
+
 }
