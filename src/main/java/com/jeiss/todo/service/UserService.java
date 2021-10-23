bd4b665267f53ffed2749d9afbcdfe205950c219
santiago cortes <jeissoncortes02@outlook.com>
2021-10-23 16:17:38 -0500

    definiendo el contrato para el usuario

--- src/main/java/com/jeiss/todo/service/UserService.java
+++ src/main/java/com/jeiss/todo/service/UserService.java
@@ -1,4 +1,13 @@
 package com.jeiss.todo.service;
 
+import com.jeiss.todo.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+    UserDto save(UserDto userDto);
+    List<UserDto> findAll();
+    UserDto findById(Long id);
+    void delete(Long id);
+    UserDto login(UserDto userDto);
 }
